f5f7cec9ad7cb5dba573ef35dc1ee686cbf8a457
Simple4Wan <simple4wan@gmail.com>
2023-11-03 17:47:36 +0800

    增加文件缓存，记录最后一次同步记录

--- main.py
+++ main.py
@@ -13,6 +13,24 @@ from onelap import onelap
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
+def get_last_sync_id(export_platform):
+    file_path = './.' + export_platform + '.tmp'
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            file_content = file.read()
+            try:
+                return int(file_content)
+            except ValueError:
+                return 0
+    return 0
+
+
+def set_last_sync_id(export_platform, activity_id):
+    file_path = './.' + export_platform + '.tmp'
+    with open(file_path, 'w') as file:
+        file.write(str(activity_id))
+
+
 def main():
     load_dotenv()
     export_platform = os.getenv('EXPORT_PLATFORM')
@@ -32,14 +50,15 @@ def main():
     export_platform_obj.login()
     import_platform_obj.login()
     # 开始同步
-    last_activity_id = 0
+    last_sync_id = get_last_sync_id(export_platform)
     while True:
         activity_list = export_platform_obj.get_activity_list()
         for activity in activity_list:
-            if activity['ride_id'] > last_activity_id:
+            if activity['ride_id'] > last_sync_id:
                 fit_file = export_platform_obj.export_fit(activity['origin_activity'])
                 import_platform_obj.import_fit(fit_file, activity['title'])
-                last_activity_id = activity['ride_id']
+                last_sync_id = activity['ride_id']
+                set_last_sync_id(export_platform, last_sync_id)
         time.sleep(60)
 
 
